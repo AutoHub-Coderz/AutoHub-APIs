314f1872a5ee4cbe4ed03d31d3295797814a0f37
Clarence Andaya <clarenceandaya8@gmail.com>
2022-07-13 16:16:02 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,14 +20,14 @@ Router::get('/info', function () {
 
 
 Router::get('/mysql', function () {
-    $mysqli = new mysqli("localhost", "autoph_api", "wefS[rLyuB.{", "autoph_api");
+    $con = mysqli_connect("localhost", "my_user", "my_password", "my_db");
 
     // Check connection
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
         exit();
     } else {
-        echo "Connected to MySQL: " . $mysqli->connect_error;
+        echo "Connected to MySQL";
         exit();
     }
-})->setName('info');
+})->setName('mysql');
