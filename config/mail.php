1d963f947225b9d162a5888207bd9ad539f782c0
Clarence Andaya <clarenceandaya8@gmail.com>
2022-07-14 13:21:54 +0800

    Create mail.php

---
+++ config/mail.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'default' => [
+        'host' => '',
+        'port' => '',
+        'username' => '',
+        'password' => '',
+        'encryption' => '',
+        'from_address' => '',
+        'from_name' => '',
+    ],
+];
