deb7a55294230f97d25dec7ae0e1877050976793
Clarence Andaya <clarenceandaya8@gmail.com>
2022-10-09 19:57:39 +0800

    Update

--- routes/api_sms.php
+++ routes/api_sms.php
@@ -24,9 +24,10 @@ Router::group(['prefix' => '/v1'], function () {
                 // //remove extra space
                 // $message = \App\Utilities\Utility::removeExtraSpace($message); 
                 // $message = strtr(input('message'), array("\n" => "\\n"));
-                $message = \App\Utilities\Utility::cleanString(input('message'));
+                $message = escape(input('message'));
+                $message = addslashes($message);
+                $message = \App\Utilities\Utility::cleanString($message);
                 $message = strtr(input('message'), array("\n" => "\\n",  "\r" => ""));
-                $message = escape($message);
 
                 $sms_config = (object) config('sms')->{$sms};
 
